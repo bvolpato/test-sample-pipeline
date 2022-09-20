7cff007092ce011253ac690381824785fe0308a0
douglascoimbra <82838240+douglascoimbra@users.noreply.github.com>
2022-09-20 17:38:58 -0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -6,4 +6,4 @@ SELECT
     CONTAINS_SUBSTR(stg.productname, 'banan') AS mdmDeleted
     --metadata{nlp,product}--
 from stg_nlp_product as stg
---timestamp-- where stg.mdmCounterForEntity > {{}}
+--timestamp-- where stg.mdmCounterForEntity > {{start_from}}
